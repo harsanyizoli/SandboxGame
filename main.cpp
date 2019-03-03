3e72ccc334d67e3169c3e173d81dd8f71477ff7b
harsanyizoli <h.zoli098@gmail.com>
2019-03-03 08:13:36 +0100

    shaders

--- main.cpp
+++ main.cpp
@@ -11,16 +11,23 @@ int SCREEN_HEIGHT = 576;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+
+    "uniform float color;\n"
+
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(ourColor.x, ourColor.y, color, ourColor.z);\n"
     "}\n\0";
 
 static void error_callback(int error, const char* description)
@@ -100,17 +107,16 @@ int main(void)
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-         0.0f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.0f, 0.0f,
-         0.0f,  0.5f, 0.0f   // top left 
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top right
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f  // top left
+         //0.0f,  0.0f, 1.0f,  1.0f, 1.0f, 0.0f
 
     };
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 4,  // first Triangle
-        1, 2, 4,   // second Triangle
-        2, 3, 4
+        0, 1, 2,  // first Triangle
+        2, 3, 0,   // second Triangle
     };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -122,10 +128,16 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
@@ -136,14 +148,18 @@ int main(void)
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
+    //glBindVertexArray(0);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
     glfwSetCursorPosCallback(window, cursor_pos_callback);
     while (!glfwWindowShouldClose(window))
     {     
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "color");
         glUseProgram(shaderProgram);
+        glUniform1f(vertexColorLocation, greenValue);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, 128, GL_UNSIGNED_INT, 0);
